e0fbd9c61872cc250f55706495022e070776b6a5
marcosprofile <marcos_laurindo1997@hotmail.com>
2024-05-06 21:52:29 -0300

    feat: fourth and fifth sections

--- src/app/section/fourth/fourth.component.ts
+++ src/app/section/fourth/fourth.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
+import { GridCardsComponent } from '../../components/grid-cards/grid-cards.component';
+import { CardComponent } from '../../components/card/card.component';
+import { TextoDestaqueComponent } from '../../components/texto-destaque/texto-destaque.component';
 
 @Component({
   selector: 'app-fourth',
   standalone: true,
-  imports: [ ContainerComponent ],
+  imports: [
+    ContainerComponent,
+    GridCardsComponent,
+    CardComponent,
+    TextoDestaqueComponent
+  ],
   templateUrl: './fourth.component.html',
   styleUrl: './fourth.component.css'
 })
